4d13c145e30d0f258f0a164827019983f292d425
saralimbu2017 <iamsebi7@gmail.com>
2021-11-26 12:33:18 +1100

    Adding route for insert operation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\User;
+use App\Models\Post;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/insert',function(){
+    $user = User::findOrFail(1);
+    $post = new Post(['title'=>'PHP','body'=>'lorem ipsum']);
+    $user->posts()->save($post);
+
 });
